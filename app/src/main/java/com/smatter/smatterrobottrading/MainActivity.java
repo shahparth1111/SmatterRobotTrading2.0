43300765555c5ed4cefc972bfa5e0b3c4c04f8cd
Parth Shah <parth.prs.shah@gmail.com>
2025-05-30 13:27:06 -0800

    UI2

--- app/src/main/java/com/smatter/smatterrobottrading/MainActivity.java
+++ app/src/main/java/com/smatter/smatterrobottrading/MainActivity.java
@@ -3,9 +3,6 @@ package com.smatter.smatterrobottrading;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
